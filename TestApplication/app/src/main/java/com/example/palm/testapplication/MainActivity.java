6d948281b898aaa765119ed3ae5eb2e7182b4123
Palmmro <palmro@student.chalmers.se>
2016-09-07 11:32:11 +0200

    change something

--- TestApplication/app/src/main/java/com/example/palm/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/palm/testapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.palm.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView frontText;
+    private Button button;
+    private boolean caps = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(){
+//        boolean caps = true;
+
+
+        caps = !caps;
+        frontText.setAllCaps(caps);
+
     }
 }
